a3510faa56836ba971f8e3a707e9af48c4844a53
lurbano <lurbano@earthsciweb.org>
2024-04-19 09:38:10 -0500

    clock update

--- clock_v8/clock_base.py
+++ clock_v8/clock_base.py
@@ -11,44 +11,47 @@ import adafruit_ntp
 import board
 import time
 from ledPixelsPico import *
+import neopixel
+
 from uNetComm import *
 from uSchedule import *
 from ledClock import *
 
 # brightness Knob
-brightness = 1.0
+brightness = 0.1
 
 #ssid, password = secrets.WIFI_SSID, secrets.WIFI_PASSWORD  # pylint: disable=no-member
 ssid, password = "TFS Students", "Fultoneagles"  # pylint: disable=no-member
 
-pool = uNetConnect(ssid, password)
-server = HTTPServer(pool)
+print("Connecting to", ssid)
+wifi.radio.connect(ssid, password)
+print("Connected to", ssid)
+
+pool = socketpool.SocketPool(wifi.radio)
+
 # get time
 #ntp = adafruit_ntp.NTP(pool, tz_offset=0)
+startTime = getNTP_Time(pool)
+now = startTime
+print("Start Time: ", startTime, startTime.hr, startTime.min, startTime.sec)
+
+# leds
 clock = ledClock(board.GP27, 20, pool, True)
 clock.initTime()
     
 
-print(f"Listening on http://{wifi.radio.ipv4_address}:80")
 # Start the server.
-server.start(str(wifi.radio.ipv4_address))
-t_zero = time.monotonic()
 
 while True:
-    
-    dt = time.monotonic() - t_zero
-    if dt >= 1.0:
-        t_zero = time.monotonic()
+    try:      
         dtime = time.monotonic() - clock.zeroTime
         clock.now = clock.startTime.addSecs(dtime)
         #print(dtime, clock.now)
-        print(f"Time: {clock.now.hr}:{clock.now.min}:{clock.now.sec}")
         clock.lightToTime(clock.now)
             
-
-
-        
-
+    except OSError as error:
+        print(error)
+        continue
 
 
 
