ef30c08091e1233cf786557f3d04be4d3fcbddda
lurbano <lurbano@earthsciweb.org>
2024-06-26 15:25:40 -0500

    added micropython dc motor

---
+++ motor-dc/micropython/code.py
@@ -0,0 +1,41 @@
+from machine import Pin, PWM
+from time import sleep
+
+IN1 = Pin(3, Pin.OUT)
+IN2 = Pin(2, Pin.OUT)
+
+speed = PWM(Pin(4))
+speed.freq(1000)
+
+while True:
+        speed.duty_u16(10000)
+        IN1.low()  #spin forward
+        IN2.high()
+        sleep(5)
+        
+        IN1.low()  #stop
+        IN2.low()
+        sleep(2)
+        
+        speed.duty_u16(20000)
+        IN1.high()  #spin backward
+        IN2.low()
+        sleep(5)
+        
+        IN1.low()  #stop
+        IN2.low()
+        sleep(2)
+    
+        speed.duty_u16(30000)
+        IN1.low()  #spin forward
+        IN2.high()
+        sleep(5)
+        
+        IN1.low()  #stop
+        IN2.low()
+        sleep(2)
+        
+        speed.duty_u16(40000)
+        IN1.high()  #spin backward
+        IN2.low()
+        sleep(5)
