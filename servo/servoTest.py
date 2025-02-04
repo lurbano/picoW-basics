ea2aa08c26cb44c2cfd11f6b977ec3cd848b71c4
lurbano <lurbano@earthsciweb.org>
2025-02-04 17:31:16 -0600

    servo

---
+++ servo/servoTest.py
@@ -0,0 +1,17 @@
+import board
+import pwmio
+import time
+from adafruit_motor import servo
+
+# create a PWMOut object on Pin A2.
+pwm = pwmio.PWMOut(board.GP26, duty_cycle=2 ** 15, frequency=50)
+# Create a servo object, my_servo.
+my_servo = servo.Servo(pwm)
+
+while True:
+    for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+        my_servo.angle = angle
+        time.sleep(0.05)
+    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
+        my_servo.angle = angle
+        time.sleep(0.05)
